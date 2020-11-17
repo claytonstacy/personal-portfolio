9f4b4744761346a7e22be54e798e8a87220f96f3
Clayton Stacy <clayton.stacy@ssandtech.com>
2020-11-17 09:56:29 -0600

    changing vuetify config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '/personal-portfolio/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/personal-portfolio/'
+    : '/',
   transpileDependencies: [
     'vuetify',
   ],
